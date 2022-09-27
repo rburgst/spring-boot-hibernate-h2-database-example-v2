c7bd159d2127aa32e376763a4b7654769c03ca65
Rainer Burgstaller <rainer.burgstaller@e-mundo.de>
2022-09-27 14:59:48 +0200

    feat: add commandline runner just for show

--- src/main/java/com/dirask/Application.java
+++ src/main/java/com/dirask/Application.java
@@ -1,10 +1,22 @@
 package com.dirask;
 
+import com.dirask.model.UserEntity;
+import com.dirask.repository.UserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public ApplicationRunner applicationRunner(UserRepository userRepository) {
+
+        return args -> {
+            userRepository.save(new UserEntity("John Doe", 42));
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
